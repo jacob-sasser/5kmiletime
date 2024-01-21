f4b44dcd003c90fcc4c2130261cacd6dce1e9c9d
jacob-sasser <jacobsasser23@gmail.com>
2024-01-21 12:18:33 -0500

    plots

--- histogram.py
+++ histogram.py
@@ -1,9 +1,9 @@
 
-'''import matplotlib.pyplot as mpl 
-import numpy
-import pandas'''
+import matplotlib.pyplot as pyplot
+import numpy as np
+import pandas
 def main():
-    data= pandas.read_csv("C:\\Users\\Jacob\\OneDrive\\Documents\\GitHub\\5ktime\\sampledata.csv")
-
+    data=pandas.read_csv("sampledata2.csv",usecols=['event_code','minutes'])
+    data.hist()
 if __name__ == '__main__':
     main()
