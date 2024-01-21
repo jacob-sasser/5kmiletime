5bdb9f1621b1ab79af21c434be77709beb0baee7
jacob-sasser <jacobsasser23@gmail.com>
2024-01-20 20:58:22 -0500

    first

---
+++ histogram.py
@@ -0,0 +1,9 @@
+
+'''import matplotlib.pyplot as mpl 
+import numpy
+import pandas'''
+def main():
+    data= pandas.read_csv("C:\\Users\\Jacob\\OneDrive\\Documents\\GitHub\\5ktime\\sampledata.csv")
+
+if __name__ == '__main__':
+    main()
