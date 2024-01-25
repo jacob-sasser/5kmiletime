b4fd8e3700fe24f9fe40e1cbfdaad8758311f994
jacob-sasser <jacobsasser23@gmail.com>
2024-01-24 20:43:24 -0500

    histogram working

--- histogram.py
+++ histogram.py
@@ -1,9 +1,16 @@
 
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas
+import pandas as pd
 def main():
-    data=pandas.read_csv("sampledata2.csv",usecols=['event_code','minutes'])
-    data.hist()
+    data = pd.read_csv("sampledata2.csv", usecols=['event_code', 'minutes'])
+
+    plt.hist([data[data.columns[0]], data[data.columns[1]]], label=[data.columns[0], data.columns[1]])
+    print(data.columns)
+    
+    plt.xlabel('')
+    plt.ylabel('Frequency')
+    #plt.legend()
+    plt.show()
 if __name__ == '__main__':
     main()
